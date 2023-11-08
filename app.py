80758fd4227c21f49e4c6a7ed3a4520c4d67315a
Irina Glimbovskaya <irishka.glimbovskaya@gmail.com>
2023-11-08 15:24:54 +0200

    Вывод списка студентов с их играми и ссылками. Изменение страинцы студента

--- app.py
+++ app.py
@@ -3,20 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/D/учеба/Flask/Flask_sait/instance/people.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/D/учеба/Flask/Flask_sait/instance/stud.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 
 
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Students(db.Model):
+    id_student = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     name_game = db.Column(db.String(100), nullable=False)
     picture = db.Column(db.String(100), nullable=False)
     href = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return '<Students %r>' % self.id
+        return '<Students %r>' % self.id_student
+
+
+class Students_web(db.Model):
+    id_student = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    photo = db.Column(db.String(100), nullable=False)
+    studies = db.Column(db.Text, nullable=False)
+    programm = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<Students_web %r>' % self.id_student
 
 
 @app.route('/')
@@ -30,10 +42,16 @@ def kurs3():
     return render_template("3kurs.html")
 
 
-@app.route('/kurs4/', methods = ['POST', 'GET'])
-def kurs4(id):
+@app.route('/kurs4/', methods=['POST', 'GET'])
+def kurs4():
+    students = Students.query.order_by(Students.id_student.asc()).all()
+    return render_template("4kurs.html", students=students)
 
-    return render_template("4kurs.html")
+
+@app.route('/kurs4/<int:id>')
+def students_web(id_student):
+    student = Students_web.query.get(id_student)
+    return render_template("students_web.html", student=student)
 
 
 @app.route('/about')
@@ -41,67 +59,5 @@ def about():
     return render_template("about.html")
 
 
-
-@app.route('/posts')
-def posts():
-    articles = Article.query.order_by(Article.date.desc()).all()
-    return render_template("posts.html", articles=articles)
-
-@app.route('/posts/<int:id>')
-def post_detail(id):
-    article = Article.query.get(id)
-    return render_template("post_detail.html", article=article)
-
-
-@app.route('/posts/<int:id>/delete')
-def post_delete(id):
-    article = Article.query.get_or_404(id)
-
-    try:
-        db.session.delete(article)
-        db.session.commit()
-        return redirect('/posts')
-
-    except Exception as e:
-        return str(e)+" При удалении поста произошла ошибка"
-
-@app.route('/posts/<int:id>/update', methods = ['POST', 'GET'])
-def post_update(id):
-    article = Article.query.get(id)
-    if request.method == "POST":
-        article.title = request.form["title"]
-        article.intro = request.form["intro"]
-        article.text = request.form["text"]
-
-        try:
-            db.session.commit()
-            return redirect('/posts')
-        except:
-            return "При редактировании статьи произошла ошибка"
-    else:
-        return render_template("post_update.html", article=article)
-
-
-@app.route('/create-article', methods = ['POST', 'GET'])
-def create_article():
-    if request.method == "POST":
-        title = request.form["title"]
-        intro = request.form["intro"]
-        text = request.form["text"]
-
-        article = Article(title=title, intro=intro, text=text)
-
-        try:
-            db.session.add(article)
-            db.session.commit()
-            return redirect('/posts')
-        except:
-            return "При добавлении статьи произошла ошибка"
-    else:
-        return render_template("create-article.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
